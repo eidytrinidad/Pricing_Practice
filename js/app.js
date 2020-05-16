b374a649626481792da688fb625817c360f34579
eidytrinidad <eidyjtf@gmail.com>
2020-05-16 01:29:47 -0400

    Adding JS code, editing slider.scss

--- js/app.js
+++ js/app.js
@@ -2,10 +2,16 @@
 function changeFunction() {
     const check = document.querySelector("#switch");
     let price =document.querySelector(".price");
+    let pbasico =document.querySelector("#pbasico");
+    let pmaster =document.querySelector("#pmaster");
     if (check.checked==true) {
-        console.log(price.textContent="$24.99")
+        price.textContent="$24.99";
+         pbasico.textContent="$19.99";
+          pmaster.textContent="$39.99";
         
     } else {
-        console.log(price.innerHTML=`$249.99`)
+        price.innerHTML=`$249.99`;
+         pbasico.textContent="$199.99";
+          pmaster.textContent="$399.99";
     }
 }
